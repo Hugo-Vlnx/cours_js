347cda1d3e649ff36f647ffda96d8faf5a87f7ea
Hugo.V <hugo.vialaneix@gmail.com>
2026-02-04 12:04:47 +0100

    tp8

---
+++ 3-Les_Conditions/monscript.js
@@ -0,0 +1,18 @@
+let number = 5;
+let text = '5';
+let isRainingToday = true;
+if(number == text){
+    console.log("true==")
+}
+else{
+    console.log("false ==");
+}
+if(number === text){
+    console.log("true===");
+}
+else{
+    console.log("false ===");
+}
+if(isRainingToday === true){
+    console.log("Where is my Umbrella");
+}
