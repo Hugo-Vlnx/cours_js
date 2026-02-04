15ef6c815825175076d52ecbc8c49896b5106730
Hugo.V <hugo.vialaneix@gmail.com>
2026-02-04 11:30:30 +0100

    TP4

---
+++ 2-Les_Variables/monscript.js
@@ -0,0 +1,7 @@
+let nomJour = "Mecredi";
+let numJour = "04";
+let nomMois = "Février";
+let isHiver = true;
+let tabJours = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+let tabSemaine = ["Janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"]
+console.log(nomJour,numJour,nomMois, isHiver);
