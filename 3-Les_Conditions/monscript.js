e3d82b6964af33a8977e31ea11bf40838f782ba5
Hugo.V <hugo.vialaneix@gmail.com>
2026-02-04 12:15:39 +0100

    tp9

--- 3-Les_Conditions/monscript.js
+++ 3-Les_Conditions/monscript.js
@@ -1,18 +1,30 @@
-let number = 5;
-let text = '5';
-let isRainingToday = true;
-if(number == text){
-    console.log("true==")
+// let number = 5;
+// let text = '5';
+// let isRainingToday = true;
+// if(number == text){
+//     console.log("true==")
+// }
+// else{
+//     console.log("false ==");
+// }
+// if(number === text){
+//     console.log("true===");
+// }
+// else{
+//     console.log("false ===");
+// }
+// if(isRainingToday === true){
+//     console.log("Where is my Umbrella");
+// }
+
+let age =prompt("Quelle âge avez-vous ?");
+
+if(age<18 && age>0){
+    console.log(`L’utilisateur a ${age} an(s), il est mineur!` );
+}
+else if(age>=18 && age<62){
+     console.log(`L’utilisateur a ${age} an(s), il est majeur!` );
 }
 else{
-    console.log("false ==");
-}
-if(number === text){
-    console.log("true===");
-}
-else{
-    console.log("false ===");
-}
-if(isRainingToday === true){
-    console.log("Where is my Umbrella");
+    console.log(`L’utilisateur a ${age} an(s), il est majeur mais aussi retraité !!` );
 }
