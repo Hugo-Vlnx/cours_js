8cfcc89dadd454dcf1f9224a36e5b7d6b582fcd5
Hugo.V <hugo.vialaneix@gmail.com>
2026-02-25 11:59:35 +0100

    tp 16 et 17

--- 6-Les_Manipulations/monscript.js
+++ 6-Les_Manipulations/monscript.js
@@ -1,11 +1,46 @@
 let dateJour = new Date(Date.now());
 let dateJourFr = dateJour.toLocaleDateString('fr-FR')
-            
-/* Toutes les variables */
 let li = document.querySelector("#listeCourses li");
 let titreH2 = document.querySelector("h2");
+let myInput = document.querySelector("#myInput");
+let btn = document.querySelector(".btn");
+let liste = document.querySelector("#listeCourses");
+
 titreH2.textContent = titreH2.textContent + " du " + dateJourFr;
-/* Tous les évènements */
+
+
 li.addEventListener('click', ()=>{
   li.classList.toggle('itemCheck');
+});
+
+btn.addEventListener('click', addProduct);
+
+function addProduct() {
+let valeurSaisie = myInput.value;
+
+    if (valeurSaisie === "") {
+        alert("Erreur");
+    } else {
+        let nouvelElement = document.createElement("li");
+        nouvelElement.textContent = valeurSaisie;
+
+        nouvelElement.addEventListener('click', () => {
+            nouvelElement.classList.toggle('itemCheck');
+        });
+
+        nouvelElement.addEventListener('dblclick', () => {
+            nouvelElement.remove();
+            alert("Produit supprimé");
+        });
+
+        liste.appendChild(nouvelElement);
+        myInput.value = "";
+    }
+
+}
+
+myInput.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        addProduct();
+    }
 });
