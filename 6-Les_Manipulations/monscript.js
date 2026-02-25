b080a1f1afd060fe1c5ef298c9ef5b9e58ce3e4b
Hugo.V <hugo.vialaneix@gmail.com>
2026-02-25 12:31:17 +0100

    maj

--- 6-Les_Manipulations/monscript.js
+++ 6-Les_Manipulations/monscript.js
@@ -1,17 +1,11 @@
 let dateJour = new Date(Date.now());
-let dateJourFr = dateJour.toLocaleDateString('fr-FR')
-let li = document.querySelector("#listeCourses li");
+let dateJourFr = dateJour.toLocaleDateString('fr-FR');
 let titreH2 = document.querySelector("h2");
 let myInput = document.querySelector("#myInput");
 let btn = document.querySelector(".btn");
 let liste = document.querySelector("#listeCourses");
 
 titreH2.textContent = titreH2.textContent + " du " + dateJourFr;
-
-
-li.addEventListener('click', ()=>{
-  li.classList.toggle('itemCheck');
-});
 
 btn.addEventListener('click', addProduct);
 
