6b9e52bdcad6b4dd2d4d05d62bb13b98f530f0d5
Hugo.V <hugo.vialaneix@gmail.com>
2026-02-25 10:11:30 +0100

    Refactor calculerAge function to use current year instead of prompting for current year

--- 5-Les_Fonctions/monscript.js
+++ 5-Les_Fonctions/monscript.js
@@ -6,10 +6,12 @@
 //     alert(` vous avez ${age}`);
 // }
 // calculerAge();
+const dateActuelle = new Date();
+const year = dateActuelle.getFullYear();
 
 const calculerAge = ()=>{
     let born = parseInt(prompt("annee de naissance ???"))
-    let year = parseInt(prompt("annee de actuelle"))
+   
     let age = year - born;
     alert(` vous avez ${age}`);
 }
